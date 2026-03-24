d106613a7cfb1db9c52f42f76c80fdf008df04f8
Aman-Bharadwaj <Amanbharadwaj110@gmail.com>
2026-03-24 22:32:08 +0530

    day_04_functions

---
+++ day04_functions.py/task2.py
@@ -0,0 +1,9 @@
+def check_even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
+print(check_even_odd(5))
+print(check_even_odd(8))
+print(check_even_odd(11))
